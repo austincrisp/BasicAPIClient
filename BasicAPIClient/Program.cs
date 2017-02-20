3d041b13197edaccb1b26a11e81b8b1cd72d1f86
austincrisp <austin.crisp05@gmail.com>
2017-02-20 16:48:35 -0500

    SetUpClient implemented

--- BasicAPIClient/Program.cs
+++ BasicAPIClient/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using BasicAPIClient.Models;
 
 namespace BasicAPIClient
 {
     class Program
     {
+        public static HttpClient client = new HttpClient();
+
+        private static void SetUpClient()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+        }
+
         static void Main(string[] args)
         {
+            SetUpClient();
         }
     }
 }
